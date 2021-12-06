c52458bc7304b4a59cd7ed5b053028e596882a91
Pedro Sturzen <pedrosturzen@gmail.com>
2021-12-06 19:26:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TiendaJS-CoderHouse-Zapatillas-
---
+++ js/main.js
@@ -0,0 +1,10 @@
+var dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]                                  
+
+var numeroDias = dias.length -1;
+var i = 0
+
+while (i <= numeroDias) {
+    alert( dias[i]);
+    console.log( "Hoy es el dia" [i]);
+    i++;
+  }
